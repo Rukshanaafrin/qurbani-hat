cf48ae39e982ec3002acfdd7e67f67dd4d35e9ea
Afrin <rukshanaafrinety797@gmail.com>
2026-05-03 01:05:14 +0600

    added to logout button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,11 @@
 "use client";
+
 import Link from "next/link";
+import { useAuth } from "@/context/AuthContext";
 
 export default function Navbar() {
+  const { user, logout } = useAuth();
+
   return (
     <div className="navbar bg-base-100 shadow-md px-6">
       <div className="flex-1">
@@ -10,11 +14,26 @@ export default function Navbar() {
         </Link>
       </div>
 
-      <div className="flex gap-4">
+      <div className="flex gap-4 items-center">
         <Link href="/" className="hover:text-green-600">Home</Link>
         <Link href="/animals" className="hover:text-green-600">All Animals</Link>
-        <Link href="/login" className="btn btn-sm btn-outline">Login</Link>
-        <Link href="/register" className="btn btn-sm btn-primary">Register</Link>
+
+        {
+          user ? (
+            <>
+              
+              <button onClick={logout} className="btn btn-sm btn-error">
+                Logout
+              </button>
+            </>
+          ) : (
+            <>
+              <Link href="/login" className="btn btn-sm btn-outline">Login</Link>
+              <Link href="/register" className="btn btn-sm btn-primary">Register</Link>
+            </>
+          )
+        }
+
       </div>
     </div>
   );
